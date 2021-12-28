e86cc433e97a651a6c7c102df25243e11d9e308f
mahmutparlak <mahmutparlak@gmail.com>
2021-12-28 01:28:30 -0600

    This is for Jenkins

---
+++ com/syntax/Main.java
@@ -0,0 +1,11 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("It will be great");
+            Thread.sleep(2000);
+        }
+    }
+}
